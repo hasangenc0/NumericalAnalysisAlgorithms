de1211dce7925b3e04958c1153023a87d2ce7597
hasan <hsnyng@outlook.com>
2019-02-16 19:14:44 +0300

    tests added

---
+++ test/Test.cpp
@@ -0,0 +1,22 @@
+#include "Test.hpp"
+#include "gtest/gtest.h"
+#include "OneVariableEquation.hpp"
+#include "BisectionMethod.hpp"
+
+typedef OneVariableEquation OVE;
+typedef BisectionMethod Bisection;
+
+TEST(SquareFuncTest, PositiveNos) {
+  EXPECT_EQ (16.0, Bisection::square (4.0));
+  EXPECT_EQ (25.0, Bisection::square (5.0));
+  EXPECT_EQ (36.0, Bisection::square (6.0));
+}
+
+int main (int argc, char *argv[]) {
+  Bisection *bisection = new Bisection();
+
+  ::testing::InitGoogleTest(&argc, argv);
+  Bisection::square(4.0);
+
+  return RUN_ALL_TESTS();
+}
---
+++ test/Test.hpp
@@ -0,0 +1,14 @@
+#ifndef Test_hpp
+#define Test_hpp
+
+namespace self {
+
+  class Test {
+    public:
+      bool succeed;
+
+  };
+
+}
+
+#endif /* Test_hpp */
