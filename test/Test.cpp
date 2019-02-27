0ce6790ffdcf8710b1245c7851b5f395202df976
hasan <hsnyng@outlook.com>
2019-02-27 23:40:22 +0300

    tests success and works for fixed point method

--- test/Test.cpp
+++ test/Test.cpp
@@ -11,20 +11,29 @@ double fn (double arg) {
   return pow(arg, 3) + 4*pow(arg,2) - 10;
 }
 
+double fixed_fn (double arg) {
+  return pow((10 - pow(arg, 3)), 0.5) * 0.5;
+}
+
 // bisection metod parameters
 int range[2] = {1, 2};
-int iterateNumber = 100;
+int iterateNumber = 10000;
 double tol = 0.0001;
 
 OneVariableFunction func(fn);
+OneVariableFunction func2(fixed_fn);
+
 BisectionMethod b1(range, iterateNumber, tol,&func);
+FixedPointMethod f1(range, iterateNumber, tol,&func2);
 
 // test class
 myTest::Test t;
 
 TEST(BisctionMethod, Test1) {
   double res = fn(b1.findRoot ());
+  double res2 = fn(f1.findRoot ());
   EXPECT_TRUE(t.isBetween(res, tol, -1*tol)) ;
+  EXPECT_TRUE(t.isBetween(res2, tol, -1*tol)) ;
 }
 
 int main (int argc, char *argv[]) {
