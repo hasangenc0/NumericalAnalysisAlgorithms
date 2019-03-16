993148d9fe95c164bfa4dfa8648c5d55ff71c078
hasan <hasangencx@hotmail.com>
2019-03-16 14:14:04 +0300

    integral simpson

--- src/Integral/integral.c
+++ src/Integral/integral.c
@@ -6,7 +6,7 @@
 #include <math.h> // for euler number
 
 // integral is in the interval 0 - 4
-const interval[2] = [0, 4];
+const int interval[2] = {0, 4};
 
 // function that we will take the integral
 double function(int x) {
@@ -17,13 +17,13 @@ double function(int x) {
 //prototypes
 void integral();
 void trapez();
-void simpson();
+double simpson();
 void simpsonThreeEight();
 void boole();
 
 int main() {
-	printf("%lf", function(2));
-	return 0;
+	integral();
+  return 0;
 }
 
 
@@ -31,8 +31,17 @@ void trapez() {
 
 }
 
-void simpson() {
-
+double simpson() {
+  double h = (interval[1] - interval[0])/2;
+  double point = 3;
+  double x0 = interval[0];
+  double x1 = x0 + h;
+  double x2 = x0 + 2*h;
+  double fx0 = function(x0);
+  double fx1 = function(x1);
+  double fx2 = function(x2);
+  printf("%lf %lf %lf\n", fx0, fx1, fx2);
+  return (h/point)*(fx0 + 4*fx1 + fx2);   
 }
 
 void simpsonThreeEight() {
@@ -44,5 +53,5 @@ void boole() {
 }
 
 void  integral() {
-
+  printf("%lf\n", simpson());
 }
