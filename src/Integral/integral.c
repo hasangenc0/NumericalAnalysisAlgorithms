6bd5dc771139141f6a678168b07207f604cb5f50
Ubuntu <ubuntu@ip-172-31-22-224.us-west-2.compute.internal>
2019-03-16 12:49:56 +0000

    all integral functions

--- src/Integral/integral.c
+++ src/Integral/integral.c
@@ -9,17 +9,17 @@
 const int interval[2] = {0, 4};
 
 // function that we will take the integral
-double function(int x) {
+double function(double x) {
 	const double Euler = exp(1.0);
 	return pow(Euler, x);
 }
 
 //prototypes
 void integral();
-void trapez();
+double trapez();
 double simpson();
-void simpsonThreeEight();
-void boole();
+double simpsonThreeEight();
+double boole();
 
 int main() {
 	integral();
@@ -27,31 +27,67 @@ int main() {
 }
 
 
-void trapez() {
+double trapez() {
+  double point = 2;
+  double h = (interval[1] - interval[0]) / (point - 1);
+  double x0 = interval[0];
+  double x1 = x0 + h;
+  double fx0 = function(x0);
+  double fx1 = function(x1);
+  printf("%lf\n", fx1);
+  return (h/point)*(fx0 + fx1);
 
 }
 
 double simpson() {
-  double h = (interval[1] - interval[0])/2;
   double point = 3;
+  double h = (interval[1] - interval[0]) / (point - 1);
   double x0 = interval[0];
   double x1 = x0 + h;
   double x2 = x0 + 2*h;
   double fx0 = function(x0);
   double fx1 = function(x1);
   double fx2 = function(x2);
-  printf("%lf %lf %lf\n", fx0, fx1, fx2);
   return (h/point)*(fx0 + 4*fx1 + fx2);   
 }
 
-void simpsonThreeEight() {
+double simpsonThreeEight() {
+  double point = 4;
+  double h = (interval[1] - interval[0]) / (point - 1);
+  double x0 = interval[0];
+  double x1 = x0 + h;
+  double x2 = x0 + 2*h;
+  double x3 = x0 + 3*h;
+  double fx0 = function(x0);
+  double fx1 = function(x1);
+  double fx2 = function(x2);
+  double fx3 = function(x3);
+  return (3*h/(2*point)) *(fx0 + 3*fx1 + 3*fx2 + fx3);   
 
 }
 
-void boole() {
+double boole() {
+  double point = 5;
+  double h = (interval[1] - interval[0]) / (point - 1);
+  double x0 = interval[0];
+  double x1 = x0 + h;
+  double x2 = x0 + 2*h;
+  double x3 = x0 + 3*h;
+  double x4 = x0 + 4*h;
+  double fx0 = function(x0);
+  double fx1 = function(x1);
+  double fx2 = function(x2);
+  double fx3 = function(x3);
+  double fx4 = function(x4);
+  return (2*h/(9*point)) *(fx0 + 32*fx1 + 12*fx2 + 32*fx3 + 7*fx4);   
+
 
 }
 
 void  integral() {
-  printf("%lf\n", simpson());
+  printf("Trapezoidal: %lf\n", trapez());
+  printf("Simpson: %lf\n", simpson());
+  printf("Simpson3/8:  %lf\n", simpsonThreeEight());
+  printf("Boole:  %lf\n", boole());
 }
+
