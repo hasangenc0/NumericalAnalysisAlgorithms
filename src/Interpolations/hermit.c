12ed37179175bf08f4cd3965305bc40d51ebce86
hasan <hasangencx@hotmail.com>
2019-03-31 23:12:28 +0300

    hermit lagrange coeffs and data file

--- src/Interpolations/hermit.c
+++ src/Interpolations/hermit.c
@@ -7,6 +7,8 @@
 // prototypes
 int hermit();
 
+const char filename[8] = "data.txt";
+
 int main() {
 	hermit();
 
@@ -14,5 +16,41 @@ int main() {
 }
 
 int hermit() {
+  int size;
+  double desired;
+  FILE *fp;
+
+  if( (fp = fopen('data.txt', 'r')) == NULL) {
+    return 0;
+  }
+
+  // get point size
+	fscanf(fp, "%d", &size);
+
+  double arr[size][size];
+
+	printf("Values:\n");
+	for (int i = 0; !feof(fp) ; i++) {
+		fscanf(fp, "%lf%lf", &arr[i][0], &arr[i][1]);
+		printf("%lf %lf \n", arr[i][0], arr[i][1]);
+	}
+
+	// close file
+	fclose(fp);
+
+  double coeff[size];
+
+  printf("Enter the Desired mid point\n");
+	scanf("%lf", &desired);
+
+	for(int i = 0; i < size; i++) {
+		coeff[i] = 1;
+		for(int k = 0; k < size; k++) {
+			if (k == i) continue;
+			coeff[i] *= (desired - arr[k][0]) / (arr[i][0] - arr[k][0]);
+		}
+		coeff[i] *= arr[i][1];
+	}
+
   return 0;
 }
